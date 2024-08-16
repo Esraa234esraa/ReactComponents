29335413cb5f3576a669063f0ef4c2d5b6462b18
esraa <eso234732@gmail.com>
2024-08-16 05:42:14 +0300

    update navbar

--- src/component/navbar/Navbar.jsx
+++ src/component/navbar/Navbar.jsx
@@ -37,10 +37,15 @@ function toggole() {
 
 window.addEventListener('scroll', function () {
     const navbar = document.querySelector('.navbar');
-    if (window.scrollY > 70) {
-        navbar.classList.add('shrink');
-    } else {
-        navbar.classList.remove('shrink');
+    if (links.classList.contains('hidden')) {
+        if (window.scrollY > 70) {
+            navbar.classList.add('shrink');
+        } else {
+            navbar.classList.remove('shrink');
+        }
+    }
+    else {
+        navbar.classList.remove('shrink')
     }
 });
 
